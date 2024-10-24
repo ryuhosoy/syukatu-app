b46be65a7c111af1acd598270b3463327f61542d
RYUHEIPC\ryuho <longpingxigu@gmail.com>
2024-10-24 11:30:03 +0900

    バックエンドurl修正

--- src/actionCalls.jsx
+++ src/actionCalls.jsx
@@ -3,7 +3,7 @@ import axios from "axios";
 export const loginCall = async (user, dispatch) => {
   dispatch({ type: "LOGIN_START" });
   try {
-    const response = await axios.post("https://syukatu-app-backend.vercel.app/api/auth/login", user);
+    const response = await axios.post("https://https://syukatu-app-backend.onrender.com//api/auth/login", user);
     dispatch({ type: "LOGIN_SUCCESS", payload: response.data });
   } catch (err) {
     dispatch({ type: "LOGIN_ERROR", payload: err });
@@ -12,7 +12,7 @@ export const loginCall = async (user, dispatch) => {
 
 export const logoutCall = async (user, dispatch) => {
   try {
-    await axios.put("https://syukatu-app-backend.vercel.app/api/auth/logout", user);
+    await axios.put("https://https://syukatu-app-backend.onrender.com//api/auth/logout", user);
     dispatch({ type: "LOGOUT" });
   } catch (err) {
     console.log(err);
